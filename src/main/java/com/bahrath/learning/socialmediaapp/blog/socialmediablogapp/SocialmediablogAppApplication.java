aeda1682895a9cea43efd8c29f58893ba0fe77cd
nehag1652 <nehag1652@gmail.com>
2024-06-19 19:03:11 -0400

    First Commit

---
+++ src/main/java/com/bahrath/learning/socialmediaapp/blog/socialmediablogapp/SocialmediablogAppApplication.java
@@ -0,0 +1,13 @@
+package com.bahrath.learning.socialmediaapp.blog.socialmediablogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediablogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediablogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bahrath/learning/socialmediaapp/blog/socialmediablogapp/SocialmediablogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahrath.learning.socialmediaapp.blog.socialmediablogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediablogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
