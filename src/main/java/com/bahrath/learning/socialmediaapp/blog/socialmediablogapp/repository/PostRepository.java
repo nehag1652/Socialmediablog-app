f4ba42de5bb57d76016f8618192564b03f90d4f4
nehag1652 <nehag1652@gmail.com>
2024-06-26 17:02:58 -0400

    pagination & Sorting

--- src/main/java/com/bahrath/learning/socialmediaapp/blog/socialmediablogapp/repository/PostRepository.java
+++ src/main/java/com/bahrath/learning/socialmediaapp/blog/socialmediablogapp/repository/PostRepository.java
@@ -2,11 +2,12 @@ package com.bahrath.learning.socialmediaapp.blog.socialmediablogapp.repository;
 
 import com.bahrath.learning.socialmediaapp.blog.socialmediablogapp.model.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface PostRepository extends JpaRepository <PostEntity,Long>{
+public interface PostRepository extends  JpaRepository <PostEntity,Long>{
 
 
 
--- src/main/java/com/bahrath/learning/socialmediaapp/blog/socialmediablogapp/service/PostService.java
+++ src/main/java/com/bahrath/learning/socialmediaapp/blog/socialmediablogapp/service/PostService.java
@@ -1,12 +1,17 @@
 package com.bahrath.learning.socialmediaapp.blog.socialmediablogapp.service;
 
 import com.bahrath.learning.socialmediaapp.blog.socialmediablogapp.dto.PostDto;
+import com.bahrath.learning.socialmediaapp.blog.socialmediablogapp.payload.PostResponse;
 
 import java.util.List;
 
 public interface PostService {
 
-    List<PostDto> getAllPosts();
+    //List<PostDto> getAllPosts();
+
+    //PostResponse getAllPosts(int pageNo, int pageSize);
+
+    PostResponse getAllPosts(int pageNo,int pageSize,String sortBy,String sortDirection);
 
     PostDto getPostById(Long id);
 
