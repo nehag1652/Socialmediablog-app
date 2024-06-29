1ddcac6b0c1e377d5bde310d5465e8b71cf12d77
nehag1652 <nehag1652@gmail.com>
2024-06-28 23:28:55 -0400

    changes

--- src/main/java/com/bahrath/learning/socialmediaapp/blog/socialmediablogapp/dto/PostDto.java
+++ src/main/java/com/bahrath/learning/socialmediaapp/blog/socialmediablogapp/dto/PostDto.java
@@ -1,14 +1,9 @@
 package com.bahrath.learning.socialmediaapp.blog.socialmediablogapp.dto;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-
 public class PostDto {
 
     private Long id;
